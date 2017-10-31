3f99327798d589e8ab564563a87c512a63f86eac
Clayton <cjsweeten101@gmail.com>
2017-10-31 13:17:34 -0600

    associations

--- db/migrate/20171031184548_create_flights.rb
+++ db/migrate/20171031184548_create_flights.rb
@@ -3,6 +3,8 @@ class CreateFlights < ActiveRecord::Migration[5.1]
     create_table :flights do |t|
       t.datetime :start
       t.datetime :end
+      t.references :from_airport
+      t.references :to_airport
 
       t.timestamps
     end
