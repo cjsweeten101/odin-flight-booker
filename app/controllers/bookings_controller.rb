204ce0afb571350e990b9980941f8ee9ff9a6e2e
Clayton <cjsweeten101@gmail.com>
2017-10-31 21:02:41 -0600

    Finish project

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,32 @@
 class BookingsController < ApplicationController
 
 	def new
+		@booking = Booking.new
+		@flight = Flight.find(white_params[:flight_id])
+		@passengers = []
+		white_params[:passengers].to_i.times do |p|
+			@passengers << Passenger.new
+		end
+	end
+
+	def create
+		flight = Flight.find(params[:booking][:flight_id])
+		booking = flight.bookings.create()
+		passengers = white_params[:booking][:passenger]
+		passengers.each do |p|
+			booking.passengers.create(name:p[:name], email:p[:email])
+		end
+
+		redirect_to booking
+	end
+
+	def show
+		@booking = Booking.find_by(id:params[:id])
+	end
+
+	private
+	def white_params
+		params.permit(:passengers, :flight_id, booking:
+			{passenger: [:name, :email]})
 	end
 end
