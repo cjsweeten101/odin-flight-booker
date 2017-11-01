be8758167309e079d84f0adb529276bf992074b7
Clayton <cjsweeten101@gmail.com>
2017-10-31 19:59:11 -0600

    passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101015530) do
+ActiveRecord::Schema.define(version: 20171101015739) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20171101015530) do
     t.datetime "updated_at", null: false
     t.index ["from_airport_id"], name: "index_flights_on_from_airport_id"
     t.index ["to_airport_id"], name: "index_flights_on_to_airport_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
 end
